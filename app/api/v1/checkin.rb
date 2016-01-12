197769c2c635a912f9f6309d659d20659bfcdbe6
adam28hung <ivseas@gmail.com>
2016-01-12 13:47:08 +0800

    add basic posts curd

--- app/api/v1/checkin.rb
+++ app/api/v1/checkin.rb
@@ -120,6 +120,7 @@ module V1
     end
 
     resource :posts do
+
       desc "List nearby posts base on user location by default range=0.5km"
       params do
         requires :device_token, type: String, desc: "device token"
@@ -141,6 +142,63 @@ module V1
       get '/inradius'  do
         authenticate!
         Post.within( params[:radius].to_i, origin: "#{params[:lat]},#{params[:lng]}")
+      end
+
+      desc "get user post"
+      params do
+        requires :device_token, type: String, desc: "device token"
+        requires :post, type: Hash do
+          requires :id, type: Integer, desc: "Post id"
+        end
+      end
+      get ':id'  do
+        authenticate!
+        user_post = current_user.posts.where(id: params['post']['id']).first
+      end
+
+      desc "create user post"
+      params do
+        requires :device_token, type: String, desc: "device token"
+        requires :post, type: Hash do
+          requires :message, type: String, desc: "message"
+          requires :site_name, type: String, desc: "site_name"
+          requires :lat, type: Float, values: -90.0..+90.0, desc: 'Current latitude.'
+          requires :lng, type: Float, values: -180.0..+180.0, desc: 'Current longitude.'
+        end
+      end
+      post ''  do
+        authenticate!
+        current_user.posts << Post.create!(params['post'])
+      end
+
+      desc "update user post"
+      params do
+        requires :device_token, type: String, desc: "device token"
+        requires :post, type: Hash do
+          requires :id, type: Integer, desc: "Post id"
+          optional :message, type: String, desc: "message"
+          optional :site_name, type: String, desc: "site_name"
+          optional :lat, type: Float, values: -90.0..+90.0, desc: 'Current latitude.'
+          optional :lng, type: Float, values: -180.0..+180.0, desc: 'Current longitude.'
+        end
+      end
+      patch ':id'  do
+        authenticate!
+        user_post = current_user.posts.where(id: params['post']['id']).first
+        user_post.update_attributes(params['post'])
+      end
+
+      desc "delete user post"
+      params do
+        requires :device_token, type: String, desc: "device token"
+        requires :post, type: Hash do
+          requires :id, type: Integer, desc: "Post id"
+        end
+      end
+      delete ':id'  do
+        authenticate!
+        user_post = current_user.posts.where(id: params['post']['id']).first
+        user_post.destroy
       end
 
     end
--- spec/app/api/v1/checkin_spec.rb
+++ spec/app/api/v1/checkin_spec.rb
@@ -133,6 +133,7 @@ RSpec.describe V1::Checkin, type: :request do
   end
 
   describe "Post endpoints" do
+
     describe "GET /api/v1/users/:device_token/posts" do
       it "list user's posts" do
         user1 = FactoryGirl.create(:user, :with_post)
@@ -172,11 +173,11 @@ RSpec.describe V1::Checkin, type: :request do
         it "has posts nearby in range 1km" do
           user1 = FactoryGirl.create(:user)
           Post.create({message: '逛夜市', site_name:'六合夜市' \
-                     , lat:22.6320758, lng:120.296966, user_id: user1.id})
+                       , lat:22.6320758, lng:120.296966, user_id: user1.id})
           expect_response = Post.create({message: '逛大街' \
-                            , site_name:'統一阪急百貨' \
-                            , lat:25.0371655, lng:121.5596045 \
-                            , user_id: user1.id})
+                                         , site_name:'統一阪急百貨' \
+                                         , lat:25.0371655, lng:121.5596045 \
+                                         , user_id: user1.id})
 
 
           params = { lat: 25.0333646, lng: 121.5637253 \
@@ -190,13 +191,13 @@ RSpec.describe V1::Checkin, type: :request do
         it "has posts nearby in range 5km" do
           user1 = FactoryGirl.create(:user)
           Post.create({message: '逛夜市', site_name:'六合夜市' \
-                     , lat:22.6320758, lng:120.296966, user_id: user1.id})
+                       , lat:22.6320758, lng:120.296966, user_id: user1.id})
           expect_response = Post.create([{message: '逛大街' \
-                            , site_name:'統一阪急百貨' \
-                            , lat:25.0371655, lng:121.5596045 \
-                            , user_id: user1.id} \
-                            ,{message: '吃咖哩', site_name:'寅樂屋咖啡咖哩小食堂' \
-                            , lat:25.0371655, lng:121.5596045, user_id: user1.id}])
+                                          , site_name:'統一阪急百貨' \
+                                          , lat:25.0371655, lng:121.5596045 \
+                                          , user_id: user1.id} \
+                                         ,{message: '吃咖哩', site_name:'寅樂屋咖啡咖哩小食堂' \
+                                           , lat:25.0371655, lng:121.5596045, user_id: user1.id}])
           params = { lat: 25.0333646, lng: 121.5637253 \
                      , device_token: user1.device_token, radius: 5}
 
@@ -208,13 +209,13 @@ RSpec.describe V1::Checkin, type: :request do
         it "has posts nearby in range 10km" do
           user1 = FactoryGirl.create(:user)
           Post.create({message: '逛夜市', site_name:'六合夜市' \
-                     , lat:22.6320758, lng:120.296966, user_id: user1.id})
+                       , lat:22.6320758, lng:120.296966, user_id: user1.id})
           expect_response = Post.create([{message: '逛大街' \
-                            , site_name:'統一阪急百貨' \
-                            , lat:25.0371655, lng:121.5596045 \
-                            , user_id: user1.id} \
-                            ,{message: '逛夜市', site_name:'樂華夜市' \
-                            , lat:25.008584, lng:121.5117573, user_id: user1.id}])
+                                          , site_name:'統一阪急百貨' \
+                                          , lat:25.0371655, lng:121.5596045 \
+                                          , user_id: user1.id} \
+                                         ,{message: '逛夜市', site_name:'樂華夜市' \
+                                           , lat:25.008584, lng:121.5117573, user_id: user1.id}])
           params = { lat: 25.0333646, lng: 121.5637253 \
                      , device_token: user1.device_token, radius: 10}
 
@@ -222,10 +223,11 @@ RSpec.describe V1::Checkin, type: :request do
           expect(response.status).to eq(200)
           expect(response.body).to eq(expect_response.to_json)
         end
+
         it "has no posts nearby" do
           user1 = FactoryGirl.create(:user)
           Post.create({message: '逛夜市', site_name:'六合夜市' \
-                     , lat:22.6320758, lng:120.296966, user_id: user1.id})
+                       , lat:22.6320758, lng:120.296966, user_id: user1.id})
           params = { lat: 25.0333646, lng: 121.5637253 \
                      , device_token: user1.device_token, radius: 10}
 
@@ -233,6 +235,63 @@ RSpec.describe V1::Checkin, type: :request do
           expect(response.status).to eq(200)
           expect(response.body).to eq([].to_json)
         end
+
+      end
+    end
+
+    describe "GET /api/v1/posts/:id" do
+      it "create user posts" do
+        user1 = FactoryGirl.create(:user, :with_post)
+        user1_post_id = user1.posts.first.id
+        Post.create({message: '逛夜市', site_name:'六合夜市' \
+                     , lat:22.6320758, lng:120.296966, user_id: user1.id})
+        params = { device_token: user1.device_token \
+                   , post: { id: user1_post_id } }
+
+        get "/api/v1/posts/#{user1_post_id}", params
+        expect(response.status).to eq(200)
+        expect(response.body).to eq(user1.posts.first.to_json)
+      end
+    end
+
+    describe "POST /api/v1/posts" do
+      it "create user posts" do
+        user1 = FactoryGirl.create(:user)
+        params = { device_token: user1.device_token \
+                   , post: {message: '逛夜市', site_name:'逢甲夜市' \
+                            , lat:24.795929, lng:120.6444043, user_id: user1.id } }
+        post "/api/v1/posts", params
+        expect(response.status).to eq(201)
+        expect(response.body).to eq(user1.posts.to_json)
+      end
+    end
+
+    describe "PATCH /api/v1/posts/:id" do
+      it "update user posts" do
+        user1 = FactoryGirl.create(:user, :with_post)
+        user1_post_id = user1.posts.first.id
+        params = { device_token: user1.device_token \
+                   , post: {id: user1_post_id, message: '吃晚餐' } }
+
+        patch "/api/v1/posts/#{user1_post_id}", params
+        expect(response.status).to eq(200)
+        expect(user1.posts.first.message).to eq('吃晚餐')
+      end
+    end
+
+    describe "DELETE /api/v1/posts/:id" do
+      it "delete user posts" do
+        user1 = FactoryGirl.create(:user, :with_post)
+        user1_post_id = user1.posts.first.id
+        expect_response = Post.create({message: '逛夜市', site_name:'六合夜市' \
+                                       , lat:22.6320758, lng:120.296966, user_id: user1.id})
+        params = { device_token: user1.device_token \
+                   , post: { id: user1_post_id } }
+
+        delete "/api/v1/posts/#{user1_post_id}", params
+        expect(response.status).to eq(200)
+        expect(user1.posts.count).to eq(1)
+        expect(user1.posts.first.message).to eq('逛夜市')
       end
     end
 
